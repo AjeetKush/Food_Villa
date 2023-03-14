32137929f5622b32eb30992f0a296f6ee4200a2c
Ajeet <ajeetkush2000@gmail.com>
2023-03-14 23:56:04 +0530

    class 1

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", { id: "title" }, "Namaste1");
+const heading2 = React.createElement("h2", { id: "title2" }, "Namaste2");
+const container = React.createElement("div", { id: "container" }, [heading, heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
