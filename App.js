5ea693ee15cf1ed7a2cd46ee04dd60a6e02aa03d
Ajeet <ajeetkush2000@gmail.com>
2023-03-16 00:42:59 +0530

    class 2

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
+
+
 const heading = React.createElement("h1", { id: "title" }, "Namaste1");
 const heading2 = React.createElement("h2", { id: "title2" }, "Namaste2");
 const container = React.createElement("div", { id: "container" }, [heading, heading2]);
