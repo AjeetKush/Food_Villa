fc42fce5dc1cd082e7c15e937b073037b8c9db1f
Ajeet <ajeetkush2000@gmail.com>
2023-03-16 23:13:58 +0530

    Class 3

--- App.js
+++ App.js
@@ -5,8 +5,28 @@ import ReactDOM  from "react-dom/client";
 
 
 
-const heading = React.createElement("h1", { id: "title" }, "Namaste1");
-const heading2 = React.createElement("h2", { id: "title2" }, "Namaste2");
-const container = React.createElement("div", { id: "container" }, [heading, heading2]);
+
+
+const heading = (<h1 id="title" key="h1">Namaste React!!!!</h1>);
+
+const HeaderComponent = () => {
+    return (<div>
+        { heading }
+        <h1>hello world!!!!</h1>
+        <h2>this is h2 tag</h2>
+    </div>
+    );
+} ;
+
+/* const HeaderComponent2 = () => (
+    <div>
+        <h1>hello world!!!!</h1>        how to write without return statment
+        <h2>this is h2 tag</h2>
+    </div>
+    ); */
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render( <HeaderComponent />);
+
