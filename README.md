5f38af91c5d047b50791d9a5961201256fe982b1
Ajeet Kumar Kushwaha <ajeetkush2000@gmail.com>
2023-12-24 20:41:46 +0530

    Episode-05

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 Namaste React 🚀
+
+/**
+ * Header {component}
+ *  - Logo
+ *  - Nav Items
+ * 
+ * Body {component}
+ *  - Search
+ *  - RestaurantContainer{component}
+ *     - RestaurantCard
+ *       - Img
+ *       - Name of Res, Star Rating, cuisine, delivery time
+ * 
+ * Footer {component}
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+
+
+ Two types of Export/Import
+
+
+- Default Export/Import
+
+export default Component;
+import Component from "path";
+
+
+- Named Export/Import
+
+export const Component;
+import {Component} from "path";
+
+
+# React Hooks
+ (Normal JS utility functions) { written by facebook developers }
+- useState() - Superpowerful State Variables in react
+- useEffect()
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,26 @@
+import { CDN_URL } from "../utils/constants";
+
+
+const RestaurantCard = (props) => {
+ 
+
+    const { resData } = props;
+
+    const {name,cuisines,avgRating,costForTwo,deliveryTime,cloudinaryImageId} = resData?.info;
+   // console.log(props); props normal js object baan ke pass hote hai... { From parent component to child component. }
+    return (
+      <div className="res-card"  style={{ backgroundColor: "#f0f0f0"}}>
+        <img className ="logo" src = { CDN_URL + cloudinaryImageId } />
+
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo}</h4>
+        <h4>{deliveryTime} minutes</h4>
+
+      </div>
+
+    );
+}
+
+export default RestaurantCard;
